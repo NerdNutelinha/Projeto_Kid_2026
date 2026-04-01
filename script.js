f22b32d35cf7a6f93d69546c11d0bb42551910ed
NerdNutelinha <geovanyalangomes15@gmail.com>
2026-04-01 13:13:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
+// CONFIG DO FIREBASE (a sua que você mandou)
+const firebaseConfig = {
+  apiKey: "AIzaSyAkE0DEk4M2cRuGV3KUft0uChblI5OEaZY",
+  authDomain: "projetokid2026.firebaseapp.com",
+  projectId: "projetokid2026",
+  storageBucket: "projetokid2026.firebasestorage.app",
+  messagingSenderId: "27516941647",
+  appId: "1:27516941647:web:faae9ea01b4aabfe84813a",
+  measurementId: "G-KDS85KX6TG"
+};
+
+// INICIAR FIREBASE
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 let tentativas = 0;
 
+// LOGIN
 function login() {
   const email = document.getElementById("email").value;
-  const senha = document.getElementById("password").value;
+  const password = document.getElementById("password").value;
   const box = document.getElementById("loginBox");
-  const error = document.getElementById("error");
 
-  // Simulação (substituir por backend real)
-  if (email === "teste@gmail.com" && senha === "1234") {
-    alert("Login realizado!");
-  } else {
-    tentativas++;
-    error.innerText = "Email ou senha incorretos";
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Login realizado!");
+    })
+    .catch((error) => {
+      tentativas++;
 
-    box.classList.add("shake");
-    setTimeout(() => box.classList.remove("shake"), 300);
+      box.classList.add("shake");
+      setTimeout(() => box.classList.remove("shake"), 300);
 
-    if (tentativas >= 3) {
-      alert("Recuperação de conta enviada (simulação)");
-      tentativas = 0;
-    }
-  }
+      if (tentativas >= 3) {
+        alert("Use recuperação de senha!");
+      }
+    });
+}
+
+// CRIAR CONTA
+function register() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Conta criada!");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+// RECUPERAR SENHA
+function recover() {
+  const email = document.getElementById("email").value;
+
+  auth.sendPasswordResetEmail(email)
+    .then(() => {
+      alert("Email de recuperação enviado!");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
 }
