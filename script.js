80151200f5425b1866f64a209f2d7738fc28cd72
NerdNutelinha <geovanyalangomes15@gmail.com>
2026-06-09 01:07:30 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    console.log("Barbearia Santa Paula carregada.");
+
+    const barbeiros = document.querySelectorAll(".barbeiro");
+
+    barbeiros.forEach(barbeiro => {
+
+        barbeiro.addEventListener("mouseenter", () => {
+            barbeiro.style.boxShadow =
+            "0 0 20px rgba(212,175,55,0.6)";
+        });
+
+        barbeiro.addEventListener("mouseleave", () => {
+            barbeiro.style.boxShadow = "none";
+        });
+
+    });
+
+});
